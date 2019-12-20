4d280ac765931c4b8e43d527c75fea8af0d2b2b6
anilmikey <56670952+anilmikey@users.noreply.github.com>
2019-12-20 15:36:25 +0530

    hello mikey

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.static('frontend'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
 app.get('/login', (req, res) => res.send('Login'))
 app.get('/register', (req, res) => res.send('register'))
-var student = {"name":"anil mikey",
+var student = {"name":"beingzero",
                 "collage":"VNRVJIET",
                 "regno": "122544647"}
 app.get('/data',(req, res)=> res.json(student))
